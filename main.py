e6ad8f20de0f72334a74201e14c0f1823064c6ee
Nafay <nafay2002rizwani@gmail.com>
2022-12-12 01:19:08 +0500

    ML Features Predictor added!

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def detectCircle(image):
             for r in range(10, 100):
                 count = 0
                 for theta in range(0, 360):
-                    x = a + r*math.cos(theta)
-                    y = b + r*math.sin(theta)
+                    x = a + r * math.cos(theta)
+                    y = b + r * math.sin(theta)
                     if image[x][y] == 255:
                         count += 1
-                if count/360 > threshold:
+                if count / 360 > threshold:
                     print("Circle detected at: ", a, b, r)
 
 
@@ -31,11 +31,12 @@ def detectLine(image):
         for c in range(0, 100):
             count = 0
             for x in range(0, image.shape[0]):
-                y = m*x + c
+                y = m * x + c
                 if image[x][y] == 255:
                     count += 1
-            if count/image.shape[0] > threshold:
+            if count / image.shape[0] > threshold:
                 print("Line detected at: ", m, c)
+
 
 def HOGFeatures(image):
     # hog features
@@ -45,17 +46,49 @@ def HOGFeatures(image):
     features = hogDescriptor.compute(image)
     return features
 
+
 def SIFTFeatures(image):
     # sift features
     sift = cv2.xfeatures2d.SIFT_create()
     keypoints1, descriptors = sift.detectAndCompute(image, None)
     return keypoints1, descriptors
 
+
 def SURFFeatures(image):
     # surf features
     surf = cv2.xfeatures2d.SURF_create()
     keypoints, descriptors = surf.detectAndCompute(image, None)
     return keypoints, descriptors
+
+
+def TrainSVM(X, Y):
+    # X: input features as vector
+    # Y: output labels as vector
+    from sklearn import svm
+    # grid search for best parameters
+    from sklearn.model_selection import GridSearchCV
+    parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10], 'gamma': [0.1, 0.01], 'degree': [2, 3]}
+    svc = svm.SVC()
+    clf = GridSearchCV(svc, parameters)
+    clf.fit(X, Y)
+    # return best estimator -> best parameters
+    return clf.best_estimator_
+
+def TrainRandomForest(X, Y):
+    # X: input features as vector
+    # Y: output labels as vector
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+    clf.fit(X, Y)
+    return clf
+
+def TrainKNN(X, Y):
+    # X: input features as vector
+    # Y: output labels as vector
+    from sklearn.neighbors import KNeighborsClassifier
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(X, Y)
+    return neigh
 
 
 
@@ -66,4 +99,3 @@ if __name__ == '__main__':
     HOGFeatures(image)
     SIFTFeatures(image)
     SURFFeatures(image)
-
