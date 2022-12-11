68cd7ae49f14bc6152c2f72f4ec10559003bdfaf
Nafay <nafay2002rizwani@gmail.com>
2022-12-12 01:42:05 +0500

    Interpolation added

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import cv2
 import math
 
+import numpy as np
+
+
+# Interpolation
+def BilinearInterpolation(image,new_size):
+    rows, cols = image.shape
+    x_ratio = float(cols) / new_size[0]
+    y_ratio = float(rows) / new_size[1]
+    new_image = np.zeros(new_size)
+    for i in range(new_size[0]):
+        for j in range(new_size[1]):
+            x = int(x_ratio * i)
+            y = int(y_ratio * j)
+            x_diff = (x_ratio * i) - x
+            y_diff = (y_ratio * j) - y
+            new_image[j][i] = (1 - x_diff) * (1 - y_diff) * image[y][x] + x_diff * (1 - y_diff) * image[y][x + 1] + (
+                        1 - x_diff) * y_diff * image[y + 1][x] + x_diff * y_diff * image[y + 1][x + 1]
+    return new_image
+
+
+def NearestNeighbourInterpolation(image, new_size):
+    # new_size: new size of image
+    # get image size
+    (h, w) = image.shape[:2]
+    # get scaling factor
+    row_ratio = w/new_size[0]
+    col_ratio = h/new_size[1]
+    # create new image
+    new_image = np.zeros(new_size)
+    # scale image
+    for i in range(0, new_size[0]):
+        for j in range(0, new_size[1]):
+            # get coordinates of nearest neighbour
+            x = math.floor(i*row_ratio)
+            y = math.floor(j*col_ratio)
+            # assign pixel value
+            new_image[j][i] = image[y][x]
+    return new_image
+
+
+
 
 def detectCircle(image):
     # parametric equation of circle
@@ -36,6 +77,39 @@ def detectLine(image):
                     count += 1
             if count / image.shape[0] > threshold:
                 print("Line detected at: ", m, c)
+
+
+def ScaleImage(image, X, Y):
+    # scale transformation matrix
+    # [ 1 0 X ]
+    # [ 0 1 Y ]
+    # [ 0 0 1 ]
+    # X: translation in x direction
+    # Y: translation in y direction
+    # scale image
+    res_image = cv2.warpAffine(image, np.float32([[1, 0, X], [0, 1, Y], [0, 0, 1]]), (image.shape[0], image.shape[1]))
+    return res_image
+
+
+def rotateImage(image, angle):
+    # rotate image
+    # get image size
+    (h, w) = image.shape[:2]
+    # calculate center of image
+    center = (w / 2, h / 2)
+    # get rotation matrix
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    # rotate image
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
+
+
+def TrainLogisticRegression(X, Y):
+    # X: input features as vector
+    # Y: output labels as vector
+    from sklearn.linear_model import LogisticRegression
+    clf = LogisticRegression(random_state=0).fit(X, Y)
+    return clf
 
 
 def HOGFeatures(image):
@@ -74,6 +148,7 @@ def TrainSVM(X, Y):
     # return best estimator -> best parameters
     return clf.best_estimator_
 
+
 def TrainRandomForest(X, Y):
     # X: input features as vector
     # Y: output labels as vector
@@ -82,6 +157,7 @@ def TrainRandomForest(X, Y):
     clf.fit(X, Y)
     return clf
 
+
 def TrainKNN(X, Y):
     # X: input features as vector
     # Y: output labels as vector
@@ -89,7 +165,6 @@ def TrainKNN(X, Y):
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(X, Y)
     return neigh
-
 
 
 if __name__ == '__main__':
