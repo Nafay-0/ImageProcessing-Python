ee5677782773e6116e8b4623c08492946a2b6569
Nafay <nafay2002rizwani@gmail.com>
2022-12-12 17:10:22 +0500

    Image Filters,Operations Added

--- main.py
+++ main.py
@@ -1,7 +1,202 @@
 import cv2
 import math
-
 import numpy as np
+
+
+
+# Guassian Filter
+def GuassianFilter(image, kernel_size, sigma):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # create kernel
+    kernel = np.zeros((kernel_size, kernel_size))
+    # calculate kernel
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            kernel[i][j] = math.exp(-((i - kernel_size / 2) ** 2 + (j - kernel_size / 2) ** 2) / (2 * sigma ** 2))
+    # normalization
+    kernel = kernel / np.sum(kernel)
+    # convolution
+    for i in range(0, h):
+        for j in range(0, w):
+            for m in range(0, kernel_size):
+                for n in range(0, kernel_size):
+                    new_image[i][j] += image[i + m][j + n] * kernel[m][n]
+    return new_image
+def MedianFilter(image,kernel_size):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # median filter
+    for i in range(0, h):
+        for j in range(0, w):
+            new_image[i][j] = np.median(image[i:i + kernel_size, j:j + kernel_size])
+    return new_image
+
+# Image Convolution
+def convolution(image, kernel):
+    # get image size
+    (h, w) = image.shape[:2]
+    # get kernel size
+    (kh, kw) = kernel.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # convolution
+    for i in range(0, h):
+        for j in range(0, w):
+            for m in range(0, kh):
+                for n in range(0, kw):
+                    new_image[i][j] += image[i + m][j + n] * kernel[m][n]
+    return new_image
+
+def Filtering(image,kernel):
+    # get image size
+    (h, w) = image.shape[:2]
+    # get kernel size
+    (kh, kw) = kernel.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # convolution
+    for i in range(0, h):
+        for j in range(0, w):
+            for m in range(0, kh):
+                for n in range(0, kw):
+                    new_image[i][j] += image[i + m][j + n] * kernel[m][n]
+    return new_image
+
+
+# Otsu Thresholding
+def OtsuThresholding(image):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # calculate histogram
+    hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+    # calculate probability
+    prob = hist / (h * w)
+    # calculate mean
+    mean = np.zeros(256)
+    for i in range(0, 256):
+        mean[i] = np.sum([j * prob[j] for j in range(0, i + 1)])
+    # calculate variance
+    variance = np.zeros(256)
+    for i in range(0, 256):
+        variance[i] = np.sum([(j - mean[i]) ** 2 * prob[j] for j in range(0, i + 1)])
+    # calculate threshold
+    threshold = np.argmax(variance)
+    # binarize image
+    for i in range(0, h):
+        for j in range(0, w):
+            if image[i][j] > threshold:
+                new_image[i][j] = 255
+            else:
+                new_image[i][j] = 0
+    return new_image
+
+# Adaptive Thresholding
+def AdaptiveThresholding(image, block_size, C):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # adaptive thresholding
+    for i in range(0, h):
+        for j in range(0, w):
+            # calculate mean
+            mean = np.mean(image[max(0, i - block_size // 2):min(h, i + block_size // 2),
+                                 max(0, j - block_size // 2):min(w, j + block_size // 2)])
+            # binarize image
+            if image[i][j] > mean + C:
+                new_image[i][j] = 255
+            else:
+                new_image[i][j] = 0
+    return new_image
+
+# Mean Filter
+def MeanFilter(image, kernel_size):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # mean filter
+    for i in range(0, h):
+        for j in range(0, w):
+            new_image[i][j] = np.mean(image[i:i + kernel_size, j:j + kernel_size])
+    return new_image
+
+# Local Mean Thresholding
+def LocalMeanThresholding(image, block_size):
+    # get image size
+    (h, w) = image.shape[:2]
+    # create new image
+    new_image = np.zeros(image.shape)
+    # local mean thresholding
+    for i in range(0, h):
+        for j in range(0, w):
+            # calculate mean
+            mean = np.mean(image[max(0, i - block_size // 2):min(h, i + block_size // 2),
+                                 max(0, j - block_size // 2):min(w, j + block_size // 2)])
+            # binarize image
+            if image[i][j] > mean:
+                new_image[i][j] = 255
+            else:
+                new_image[i][j] = 0
+    return new_image
+
+#Histogram Equalization
+def HistogramEqualization(image):
+    # get image size
+    (h, w) = image.shape[:2]
+    # get histogram
+    hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+    # calculate cdf
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max() / cdf.max()
+    # create new image
+    new_image = np.zeros(image.shape)
+    # equalize image
+    for i in range(0, h):
+        for j in range(0, w):
+            new_image[i][j] = cdf_normalized[image[i][j]]
+    return new_image
+
+
+# Histogram of Oriented Gradients
+def HOG(image):
+    # get image size
+    (h, w) = image.shape[:2]
+    # calculate gradient
+    gx = cv2.Sobel(np.float32(image), cv2.CV_64F, 1, 0, ksize=1)
+    gy = cv2.Sobel(np.float32(image), cv2.CV_64F, 0, 1, ksize=1)
+    mag, ang = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+    # quantizing binvalues in (0...16)
+    bins = np.int32(16 * ang / (2 * np.pi))
+    # Divide to 4 sub-squares
+    bin_cells = bins[:10, :10], bins[10:, :10], bins[:10, 10:], bins[10:, 10:]
+    mag_cells = mag[:10, :10], mag[10:, :10], mag[:10, 10:], mag[10:, 10:]
+    hists = [np.bincount(b.ravel(), m.ravel(), 16) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)
+    return hist
+
+# IOU calculation
+def IOU(box1, box2):
+    # box1: (x1, y1, x2, y2)
+    # box2: (x1, y1, x2, y2)
+    # calculate intersection
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+    intersection = (x2 - x1) * (y2 - y1)
+    # calculate union
+    union = (box1[2] - box1[0]) * (box1[3] - box1[1]) + (box2[2] - box2[0]) * (box2[3] - box2[1]) - intersection
+    # calculate IOU
+    iou = intersection / union
+    return iou
 
 
 # Interpolation
